3691ef6513e01fd8d12d3ab3db5baad2d7fd52d4
Wisha <nazwishapsc@gmail.com>
2026-02-06 22:59:51 +0500

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./app.scss"
 import Dock from "./components/Dock"
+import Nav from "./components/Nav"
 
 const App = () => {
   return (
     <div>
       <main>
         <Dock/>
+        <Nav/>
       </main>
     </div>
   )
